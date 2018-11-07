e61594068887547effd42c88870cc776c6232e68
AnanyaCN <ananya.aus@gmail.com>
2018-11-07 09:14:22 +0100

    Resolved pagination restriction from server side by iterating over next links in bundle

--- Patient_Cohort_Counter.py
+++ Patient_Cohort_Counter.py
@@ -10,47 +10,56 @@ import json
 from fhirclient.models import bundle
 # Connection settings to server
 
-server1 = {
+server = {
     'app_id': 'my_web_app',
-    'api_base': 'http://hapi.fhir.org/baseDstu3'
+    'api_base': 'http://hapi.fhir.org/baseDstu3/'
 }
 
-server2 = {
-    'app_id': 'my_app',
-    'api_base': 'https://syntheticmass.mitre.org/fhir'
-}
+baseUrl= server['api_base']
+smart = client.FHIRClient(settings=server)
 
-smart = client.FHIRClient(settings=server1)
-smart2 = client.FHIRClient(settings=server2)
-count1 = 0
-count2 = 0
-age_i = 0
-age = 0
-num = 0
 
-def perform_in(srch_str, server):
+def perform_in(searchString, server, apiBase):
     """ Execute the search URL against the given server.
 
     :param server: The server against which to perform the search
     :returns: A Bundle resource
     """
 
-    from fhirclient.models import bundle
     if server is None:
         raise Exception("Need a server to perform search")
-
-    res = server.request_json(srch_str)
-    bundle = bundle.Bundle(res)
-    bundle.origin_server = server
-
     resources = []
+    bundleBase = server.request_json(searchString)
+    bundleCur = bundleBase
+
+    from fhirclient.models import bundle
+    bundle = bundle.Bundle(bundleBase)
+    bundle.origin_server = server
     if bundle is not None and bundle.entry is not None:
         for entry in bundle.entry:
             resources.append(entry.resource)
+
+    while True:
+        if bundleCur['link'][1]['relation'] != 'previous':
+            from fhirclient.models import bundle
+            bundleAll = list()
+            url = bundleCur['link'][1]['url']
+            urlString = url.replace(apiBase, '')
+            bundleNext = server.request_json(urlString)
+            bundleN = bundle.Bundle(bundleNext)
+            bundleN.origin_server = server
+            if bundleN is not None and bundleN.entry is not None:
+                  for entry in bundleN.entry:
+                         resources.append(entry.resource)
+            bundleCur = bundleNext
+        else:
+            break
+
     return resources
 
 #Patients diagnosed with diabetes
-search_str = 'Patient?_has:Condition:subject:code:73211009,399144008,11530004,9414007,315051004,9414007,15771004,15777000,44054006'
+#search_str = 'Patient?_has:Condition:subject:code:73211009,399144008,11530004,9414007,315051004,9414007,15771004,15777000,44054006'
+search_str='Condition?_include=Condition:patient&code=195917001'
 
 #Patients diagnosed with sinusitis
 #search_str = 'Patient?_has:Condition:subject:code=444814009,75498004,36971009,40055000'
@@ -58,20 +67,8 @@ search_str = 'Patient?_has:Condition:subject:code:73211009,399144008,11530004,94
 #Patients diagnosed with asthma
 #search_str = 'Patient?_has:Condition:subject:code=195917001'
 
-bundle1 = perform_in(search_str,smart.server)
-bundle2 = perform_in(search_str,smart2.server)
-
-cohortSize = len(bundle1) + len(bundle2)
-
-if len(bundle1)!= 0:
-    print("Cohorts retrieved from smart server is: %s" %len(bundle1))
-else:
-    print('No resources retrieved from Smart server')
-
-if len(bundle2)!=0:
-    print("Cohorts retrieved from smart server is: %s" % len(bundle2))
-else:
-    print('No resources retrieved from Synthetic mass server')
+bundle = perform_in(search_str,smart.server,baseUrl)
+cohortSize = len(bundle)/2
 
 ageSum=0
 
@@ -83,26 +80,19 @@ def calculate_age(born):
 
 print("Cohort Size %s" %cohortSize)
 
+
 # Loop over patients in bundle
-for patients in bundle1:
-    entry = patients.as_json()
-    if 'birthDate' in entry.keys():
-        dob = entry['birthDate']
-        age_i = calculate_age(dob)
-        ageSum = ageSum+age_i
-    else:
-        cohortSize =cohortSize-1
+for resources in bundle:
+    json_res = resources.as_json()
 
-for patients in bundle2:
-    entry = patients.as_json()
-    if 'birthDate' in entry.keys():
-        dob = entry['birthDate']
+    if 'birthDate' in json_res.keys():
+        dob = json_res['birthDate']
+        if 'deceasedBoolean' in json_res.keys():
+            if 'deceasedDateTime' in json_res.keys():
+                dod = json_res['deceasedDateTime']
         age_i = calculate_age(dob)
-        ageSum = ageSum+age_i
-    else:
-        cohortSize =cohortSize-1
+        ageSum = ageSum + age_i
 
-print("Cohort size after eliminating patients with no age information: %s" % cohortSize)
 
 # Calculate mean age
 meanAge = None
